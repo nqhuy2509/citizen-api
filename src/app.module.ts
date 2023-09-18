7a192d3b6e40e6dc174d07a7a07a37b3bd97ba3b
nqhuy2509 <nqhuy250901@gmail.com>
2023-09-18 22:14:38 +0700

    update connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { ProfileModule } from './profile/profile.module';
 		ConfigModule.forRoot({ isGlobal: true }),
 		TypeOrmModule.forRoot({
 			type: 'postgres',
-			host: 'localhost',
+			host: process.env.DATABASE_HOST,
 			port: parseInt(process.env.DATABASE_PORT),
 			username: process.env.DATABASE_USERNAME,
 			password: process.env.DATABASE_PASSWORD,
